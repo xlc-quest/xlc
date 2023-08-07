06089e4c78f3e59a164ed93135164005d3a2139d
xlc <eddie.cha@outlook.com>
2023-08-07 05:45:26 -0700

    simplified env

--- src/server/env.ts
+++ src/server/env.ts
@@ -1,4 +1,3 @@
-const IS_PROD = true;
 const IS_CONNECTION_SERVER = true;
 
 export const CONNECTION_SERVER_ID = '@connections';
@@ -7,5 +6,5 @@ export const PUBLIC_PORTS = ['8080', '80'];
 export const DEV_PORTS = ['3000', '3001', '3002'];
 
 export const SERVER_PORT = process.env.PORT || process.argv[2] || '3000';
+export const SERVER_URL = process.env.PORT ? 'https://xlc.quest' : `http://localhost:${SERVER_PORT}`;
 export const SERVER_ID = PUBLIC_PORTS.includes(SERVER_PORT) || IS_CONNECTION_SERVER ? CONNECTION_SERVER_ID : '@server:' + SERVER_PORT;
-export const SERVER_URL = IS_PROD ? 'https://xlc.quest' : `http://localhost:${SERVER_PORT}`;
