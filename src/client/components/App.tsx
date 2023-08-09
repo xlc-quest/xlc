c0c45064cb83f76680c85d875b79a5d8f12ce76c
xlc <eddie.cha@outlook.com>
2023-08-09 01:59:59 -0700

    implemented filtering ux

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -9,7 +9,7 @@ import { NeutralColors } from '@fluentui/theme';
 import { DefaultPalette } from '@fluentui/react/lib/Styling';
 import { DefaultButton, IconButton, PrimaryButton } from '@fluentui/react/lib/Button';
 
-import axios from 'axios';
+import axios, { all } from 'axios';
 import { useState, useEffect, Dispatch, createContext, useContext } from 'react';
 import { SendCalloutBtn } from './SendPanelBtn';
 import { configs } from '../configs';
@@ -34,35 +34,61 @@ export const ConnectionsContext = createContext({
     setClient: setClientFunc
 });
 
+interface Transaction {
+	id?: string,
+	time?: string,
+	from?: string,
+	to?: string,
+	amount?: string,
+	message?: string
+}
+
 //const transactions = [];
 const App = (props: AppProps) => {
-	const [transactions, setTransactions] = useState<any[]>([]);
+	const [allTransactions, setAllTransactions] = useState<Transaction[]>([]);
+	const [filter, setFilter] = useState('');
+	const [filteredTransactions, setFilteredTransactions] = useState<any[]>(allTransactions);
 	const [client, setClient] = useState(connections.client);
 
 	const columns = [
-		{ key: 'id', name: 'transaction#', fieldName: 'id', 		minWidth: 50, maxWidth: 220, isResizable: true },
-		{ key: 'time', name: 'time', fieldName: 'time', 			minWidth: 50, maxWidth: 100, isResizable: true },
+		{ key: 'id', name: 'transaction#', fieldName: 'id', 		minWidth: 50, maxWidth: 250, isResizable: true },
+		{ key: 'time', name: 'time', fieldName: 'time', 			minWidth: 50, maxWidth: 150, isResizable: true },
 		{ key: 'from', name: 'from', fieldName: 'from', 			minWidth: 50, maxWidth: 100, isResizable: true },
 		{ key: 'to', name: 'to', fieldName: 'to', 					minWidth: 50, maxWidth: 100, isResizable: true },
-		{ key: 'amount', name: 'amount', fieldName: 'amount', 		minWidth: 50, maxWidth: 100, isResizable: true },
+		{ key: 'amount', name: 'amount', fieldName: 'amount', 		minWidth: 50, maxWidth: 80, isResizable: true },
 		{ key: 'message', name: 'message', fieldName: 'message', 	minWidth: 200, maxWidth: 500, isResizable: true },
 	];
 
 	const refreshTransactionsAsync = () => {
 		axios
-		.get(`${configs.serverUrl}/transactions?id=client`)
+		.get(`${configs.serverUrl}/transactions?id=${client.id}`)
 		.then((res) => {
 			if (res.data.length <= 0) return;
-			setTransactions((prevList) => [...res.data.reverse()]);
 			connections.updateBalance(res.data);
+
+			const transactions: Transaction[] = [];
+			res.data.forEach((t: any) => {
+				transactions.push({
+					id: t.id,
+					from: t.from,
+					to: t.to,
+					time: new Date(t.time).toLocaleString(),
+					message: t.message,
+					amount: 'x$'+Number(t.amount).toFixed(4)
+				})
+			});
+
+			setAllTransactions(transactions.reverse());
+			setFilteredTransactions(transactions);
 		});
 	}
 
 	const refreshBalance = () => {
-		connections.updateBalance(transactions);
+		connections.updateBalance(filteredTransactions);
 	}
 
 	const _onClickRefresh = () => {
+		setFilter('');
 		refreshTransactionsAsync();
 	}
 
@@ -73,7 +99,7 @@ const App = (props: AppProps) => {
 
 	return (
 	<ConnectionsContext.Provider value={{ client, setClient }}>
-		<Stack enableScopedSelectors styles={stackStyles} tokens={verticalGapStackTokens}>
+		<Stack enableScopedSelectors styles={stackStyles} tokens={verticalGapStackTokens} horizontalAlign='end'>
 			<h1 style={{ fontSize:'24px', lineHeight:'24px', textAlign:'right', margin: 0, padding: 0, color: NeutralColors.gray120 }}>..xlc</h1>
 			<Stack horizontal horizontalAlign='end' style={{color:'gray'}}>
 				<Text size={100}>..<span style={{color: 'violet'}}>{client.ip}</span></Text>
@@ -95,23 +121,55 @@ const App = (props: AppProps) => {
 				<SettingsDlgBtn refreshBalance={refreshBalance}></SettingsDlgBtn>
 				<SendCalloutBtn refreshTransactionsAsync={refreshTransactionsAsync} />
 			</Stack>
-			<Stack horizontal horizontalAlign='end'>
-			{ transactions.length > 0 ? (
-				<div style={{width:'100%'}}>
-				<Text size={200} style={{color:'gray'}}>showing {transactions.length+'/'+transactions.length} transactions..</Text>
-				<DetailsList
-					items={transactions}
-					columns={columns}
-					selectionMode={SelectionMode.none}
-					layoutMode={DetailsListLayoutMode.justified}
-					ariaLabelForSelectionColumn="Toggle selection"
-					ariaLabelForSelectAllCheckbox="Toggle selection for all items"
-					checkButtonAriaLabel="select row"
-				/></div>) : (
-					<Text size={200} style={{color:'gray'}}>loading.. hit 'refresh' if it persists..</Text>
-				)
-			}
-			</Stack>
+			
+		{ allTransactions.length > 0 ? (
+			<Stack horizontalAlign='end' style={{width:'100%'}}>
+				<Stack horizontal horizontalAlign='end'>
+					<Stack>
+					</Stack>
+					<Stack style={{ maxWidth:'400px' }}>
+						<TextField
+							placeholder='search for transaction#, from, to, amount, message'
+							prefix='filter..'
+							defaultValue={filter}
+							size={200}
+							onChange={((e, v) => {
+								if (!v) {
+									setFilteredTransactions(allTransactions);
+								} else {
+									setFilteredTransactions(allTransactions.filter(t =>
+										t.id?.includes(v) ||
+										t.from?.includes(v) ||
+										t.to?.includes(v) ||
+										t.amount?.includes(v) ||
+										t.message?.includes(v)));
+								}
+
+								setFilter(filter);
+							})}
+							style={{ fontSize:'90%', textAlign:'right'}}
+						/>
+					</Stack>
+				</Stack>
+				<Stack>
+					<Text size={200} style={{color:'gray'}}>..showing {filteredTransactions.length+'/'+allTransactions.length}</Text>
+				</Stack>
+				<Stack style={{width:'100%'}}>
+					<DetailsList
+						compact={true}
+						items={filteredTransactions}
+						columns={columns}
+						selectionMode={SelectionMode.none}
+						layoutMode={DetailsListLayoutMode.justified}
+						ariaLabelForSelectionColumn="Toggle selection"
+						ariaLabelForSelectAllCheckbox="Toggle selection for all items"
+						checkButtonAriaLabel="select row"
+						/>
+				</Stack>
+			</Stack>) : (
+			<Text size={200} style={{color:'gray'}}>loading.. hit 'refresh' if it persists..</Text>
+			)
+		}
 		</Stack>
 	</ConnectionsContext.Provider>
 	);
