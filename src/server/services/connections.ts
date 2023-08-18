469280e8426862a2106b78f24613afada4c56a6d
xlc <contact@xlc.quest>
2023-08-18 00:57:03 -0700

    setting up the first connection only once.

--- src/server/services/connections.ts
+++ src/server/services/connections.ts
@@ -25,7 +25,7 @@ export function _extendConnections(id: string, url?: string): Connection[] {
   }
 
   const now = new Date().getTime();
-  if (id == env.CONNECTION_SERVER_ID && connections[0].url == env.SERVER_URL) {
+  if (connections.length == 1) {
     console.log(`setting up the first @connections.. without expiry`);
     connections[0].registeredTime = connections[0].registeredTime || now;
     return connections;
